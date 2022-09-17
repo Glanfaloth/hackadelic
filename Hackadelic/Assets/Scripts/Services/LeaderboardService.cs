20e1cf36ecedfd1b2cc19ba9823e4c28e0e15111
loewsi <loewsi@ethz.ch>
2022-09-17 15:06:12 +0200

    leaderboard service done

--- Hackadelic/Assets/Scripts/Services/LeaderboardService.cs
+++ Hackadelic/Assets/Scripts/Services/LeaderboardService.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
@@ -16,24 +17,13 @@ public class LeaderboardService : Service
         for (int i = 0; i < scores_list.Length; i++) {
             scores.Add(scores_list[i]);
         }
-        Debug.Log(scores.Count);
-
     }
 
     public void AppendToLeardboard(Score score)
     {
-        // Debug.Log(scores.Count);
-        // scores.Append(score);
-        // Debug.Log(scores.Count);
-
-
-        // var filePath = $"{Application.dataPath}/Database/leaderboard.json";
-        // Debug.Log(scores[1].totalTime);
-        // string jsonObject = JsonUtility.ToJson(scores);
-        // Debug.Log(jsonObject);
-
-
-
+        scores.Add(score);
+        string jsonObject = JsonConvert.SerializeObject(new { scores = scores });
+        System.IO.File.WriteAllText(@$"{Application.dataPath}/Database/leaderboard.json", jsonObject);
     }
 
 }
