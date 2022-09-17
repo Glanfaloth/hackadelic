33ee81103b0c08d582b297b4ea5f35933c732c32
Glanfaloth <yeltao@student.ethz.ch>
2022-09-17 22:05:18 +0200

    move elevator

--- elevator/src/components/Elevator.js
+++ elevator/src/components/Elevator.js
@@ -1,7 +1,15 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
 import logo from "../assets/schindlerlogo.png";
 export default function Elevator({}) {
-  
+  const [height, setHeihgt] = useState(20);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHeihgt((e) => Math.min(e + 10, 640));
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <div
       style={{
@@ -11,10 +19,10 @@ export default function Elevator({}) {
         position: "absolute",
         right: "20px",
         zIndex: "4",
-        border: "2px solid",
+        border: "5px solid",
         borderRadius: "10px",
-        borderColor: "red",
-        bottom: "20px",
+        borderColor: height < 640 ? "red" : "green",
+        bottom: `${height}px`,
         padding: "5px",
         alignContent: "center"
       }}
